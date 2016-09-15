d19d908402d30e78e6b4327be4bdaf41d531b0ff
Kanduru, Ajay (NIH/NLM/LHC) [C] <akanduru@mail.nih.gov>
2016-09-15 12:14:08 -0400

    Update lforms required version and fix an element finder for loinc code link in protractor test

--- test/protractor/spec/demopage.po.js
+++ test/protractor/spec/demopage.po.js
@@ -10,7 +10,7 @@ var DemoPage = function() {
     helpButton: element.all(by.css('button.help-button')).first(),
     instructions: element(by.css('div.popover-inner')),
     displayCodeCheckbox: element.all(by.css('div.col-md-3.col-xs-3 label input[type="checkbox"]')).first(),
-    loincCodeLabel: element(by.cssContainingText('div.name-label > span.item-code > span', '[66773-3]')),
+    loincCodeLabel: element(by.cssContainingText('div.name-label > span.item-code > a', '[66773-3]')),
     itemIntType: element(by.id('/Your_Disease_History/-dZecSUoNVD/1/1')),
     itemNumberType: element(by.id('/Your_Health_Information/z93OErjdWOh/1/1')),
     itemRequired: element(by.id('/Your_Health_Information/tXwaaWzu1Od/1/1')),
